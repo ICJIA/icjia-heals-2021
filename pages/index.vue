d5a367e825bd2c77dacc4e9129f59c4670948d4b
Chris Schweda <cschweda@gmail.com>
2020-11-03 14:48:05 -0600

    fix: scaffold out news and pubs for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <client-only>
       <Splash></Splash>
-      <v-container v-if="doc">
+      <v-container v-if="doc" fluid>
         <v-row>
           <v-col
             cols="12"
@@ -25,9 +25,28 @@
         <Loader></Loader>
       </v-container>
       <div
-        style="background: #eee; min-height: 300px; border-top: 1px solid #ccc"
+        style="
+          background: #fafafa;
+          min-height: 300px;
+          border-top: 1px solid #ccc;
+        "
       >
-        News here
+        <v-container fluid>
+          <v-row>
+            <v-col v-if="news" cols="12" md="7" class="markdown-body">
+              <h2>News and Updates</h2>
+              <div class="mt-8">
+                {{ news }}
+              </div>
+            </v-col>
+            <v-col cols="12" md="5" class="markdown-body"
+              ><h2>Latest Publications</h2>
+              <div class="mt-8">
+                {{ publications }}
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
       </div>
     </client-only>
   </div>
@@ -39,18 +58,22 @@ import { handleClicks } from '@/mixins/handleClicks'
 export default {
   mixins: [handleClicks],
   async asyncData({ $content, params }) {
-    const now = new Date().toJSON().split('T')[0]
     const doc = await $content('index').fetch()
-    const news = await $content('news')
-      .only(['title', 'description', 'scheduled', 'slug', 'markdown', 'path'])
-      .where({ scheduled: { $gt: now } })
-      .sortBy('scheduled', 'desc')
+    let news = await $content('news')
+      .only(['title', 'description', 'posted', 'slug', 'markdown', 'path'])
+      .limit(2)
+      .sortBy('posted', 'desc')
       .fetch()
-    const newsFinal = news.map((item) => ({
+    news = news.map((item) => ({
       ...item,
       show: false,
     }))
-    return { doc, newsFinal }
+    const publications = await $content('publications')
+      .only(['title', 'description', 'posted', 'slug', 'path'])
+      .sortBy('scheduled', 'desc')
+      .fetch()
+
+    return { doc, news, publications }
   },
   data() {
     return {
