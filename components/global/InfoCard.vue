0565822426f89c220463e4ac0a90f307c812e013
Chris Schweda <cschweda@gmail.com>
2020-11-07 12:54:17 -0600

    fix: add placeholder spinners

--- components/global/InfoCard.vue
+++ components/global/InfoCard.vue
@@ -27,7 +27,7 @@
           <v-row class="fill-height ma-0" align="center" justify="center">
             <v-progress-circular
               indeterminate
-              color="grey lighten-5"
+              color="blue darken-3"
             ></v-progress-circular>
           </v-row>
         </template>
--- components/global/Splash.vue
+++ components/global/Splash.vue
@@ -36,8 +36,16 @@
             </h3>
           </v-col></v-row
         ></v-container
-      ></v-img
-    >
+      >
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular
+            indeterminate
+            color="gray lighten-5"
+          ></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
   </div>
 </template>
 
