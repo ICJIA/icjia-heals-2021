b6a22e71eb519a28f99c40641b8123cdc18397aa
cschweda <cschweda@gmail.com>
2021-04-27 08:20:46 -0500

    fix: temp sandbox

---
+++ pages/sandbox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="margin-top: 100px">
+    <div class="background-example" style="height: 400px; width: 100%">
+      <div class="">
+        <div class="row d-flex align-items-center">
+          <div class="col-md-7"></div>
+          <div
+            class="col-md-5"
+            style="background: rgba(0, 0, 0, 0.7); height: 400px"
+          >
+            <h1
+              class="display-4 mt-n2 text-shadow text-center"
+              style="color: #fff"
+            >
+              Illinois HEALS
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.background-example {
+  background-image: url('https://ilheals.com/img/heart-resized.jpg');
+  background-color: gray;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  opacity: 0.9;
+}
+</style>
